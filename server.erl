bca7d9bd4ab9596bab6bc54e9be0cf25af409031
Alexey Astafyev <av.astafyev@gmail.com>
2012-08-20 23:51:15 +0400

    added skel of TCP server

---
+++ server.erl
@@ -0,0 +1,23 @@
+-module(server).
+-author('Alexey V. Astafyev <av.astafyev@gmail.com>').
+-export([listen/1]).
+
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
+
+listen(Port) ->
+  {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
+  accept(LSocket).
+
+accept(LSocket) ->
+  {ok, Socket} = gen_tcp:accept(LSocket),
+  spawn(fun() -> loop(Socket) end),
+  accept(LSocket).
+
+loop(Socket) ->
+  case gen_tcp:recv(Socket, 0) of
+    {ok, Data} ->
+      gen_tcp:send(Socket, Data),
+      loop(Socket);
+    {error, closed} ->
+      ok
+  end.
